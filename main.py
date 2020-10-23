fba0e39eaaf4036a53caddafd94bc18ee9c269c7
= <renatoversianidrakk@gmail.com>
2020-10-23 13:17:37 -0300

    Handle_user_input improved

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+from exceptions import IncorrectUsageException
+
+
 # TODO: Implement the winning / loosing logic
 # TODO: Implement the switch player logic
 # TODO: Debug and clean the code
 
 # Initialize the field
 field = []
+players = ['X', 'O']
+
+# if count is odd,  the player will be 'O'
+# if count is even, the player will be 'X'
+count = 0
 
 
 # Takes the field array as parameter
@@ -17,22 +25,27 @@ def get_user_input():
     return input('1 to 9: ')
 
 
-# Returns True if everything is okay, False, if some error found
+# Returns True if everything is okay, raises an exception, if some error found
 def check_user_input(input_=''):
-    # '', < 8, > 0, ['X']
+    # Possible Error: empty value: ''
+    # Possible Error: value is bigger than 8
+    # Possible Error: value is smaller than 0
+    # Possible Error: value is already filled
+
     if input_ == '':
-        return False
+        raise IncorrectUsageException('Input cannot be empty!')
 
     if not input_.isnumeric():
-        return False
+        raise IncorrectUsageException('Input must be a string!')
 
     integer_input = int(input_)
     if not 9 >= integer_input >= 1:
-        return False
+        raise IncorrectUsageException('Input must be bigger than or equals to 1! \n' +
+                                      'Input must be smaller than or equals to 9')
 
     # -1 is the standard input decrease value
     if field[integer_input - 1] != ' ':
-        return False
+        raise IncorrectUsageException('Input cannot be place in an already filled spot!')
 
     return True
 
@@ -49,29 +62,34 @@ def draw_field(field_):
     print(temp_field)
 
 
-def handle_incorrect_user_input():
-
+def handle_user_input():
+    input_ = get_user_input()
     while True:
-        print('Incorrect user input!')
+        try:
+            check_user_input(input_)
+            return input_
+        except IncorrectUsageException as e:
+            print(e.message)
 
-        new_input = get_user_input()
+            input_ = get_user_input()
 
-        if check_user_input(new_input):
-            return new_input
+
+def get_player(player_count):
+    if (player_count % 2) == 0:
+        return players[0]
+    return players[1]
 
 
 if __name__ == '__main__':
+
     initialize_field(field)
 
     while True:
-        user_input = get_user_input()
+        user_input = handle_user_input()
 
-        if not check_user_input(user_input):
-            user_input = handle_incorrect_user_input()
+        # -1 allows the user to play with 1 ~ 9 index
+        field[int(user_input) - 1] = get_player(count)
 
-        field[int(user_input) - 1] = 'X'
+        count += 1
 
         draw_field(field)
-
-
-# Turn of the other player
